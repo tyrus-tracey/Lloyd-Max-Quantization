a00ef40afcaf10d1fbd56d27c978434643fdd601
T <ttracey@sfu.ca>
2020-08-10 12:16:16 -0700

    completed.

--- Lloyd-Max Quantization/Lloyd-Max Quantization.cpp
+++ Lloyd-Max Quantization/Lloyd-Max Quantization.cpp
@@ -11,6 +11,7 @@ int main()
     while (running) {
         std::cout << "Enter integer N, followed by 3 floats y1 y2 y3 (Example: 1 -0.5 0 0.5). X to close:\n";
         std::getline(std::cin, inputString);
+
         if (inputString != "X" && inputString != "x") {
             std::stringstream inputStream(inputString);
             lloydMax L(inputStream);
